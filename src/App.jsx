ed6fb27ce3f0599bfe0275b8fc1be97f2f697892
Keith Lamb <keithianlamb@gmail.com>
2025-04-16 13:05:45 -0500

    Finish TailwindCss Package and Setting up Stlying

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,15 @@ import { Outlet, Link } from 'react-router-dom'
 
 export default function App() {//Nav Bar Shown on Top for home page
   return (
-    <div className="min-h-screen bg-white text-gray-900">
-      <nav className="flex justify-between items-center p-4 bg-blue-600 text-white">
-        <h1 className="text-xl font-bold">BrightPath Electric</h1>
+    <div className="min-h-screen bg-gray-100 text-gray-900">
+      <nav className="flex justify-between items-center p-6 bg-green-500 text-white"> {/* Changed background color */}
+        <h1 className="text-10xl font-bold">South Volt Electric</h1>
         <div className="space-x-4">
-          <Link to="/">Home</Link>
-          <Link to="/services">Services</Link>
-          <Link to="/faq">FAQ</Link>
-          <Link to="/about">About</Link>
-          <Link to="/contact">Contact</Link>
+          <Link to="/" className="hover:bg-green-700 p-2 rounded">Home</Link> {/* Added hover background */}
+          <Link to="/services" className="hover:bg-green-700 p-2 rounded">Services</Link> {/* Added hover background */}
+          <Link to="/faq" className="hover:bg-green-700 p-2 rounded">FAQ</Link> {/* Added hover background */}
+          <Link to="/about" className="hover:bg-green-700 p-2 rounded">About</Link> {/* Added hover background */}
+          <Link to="/contact" className="hover:bg-green-700 p-2 rounded">Contact</Link> {/* Added hover background */}
         </div>
       </nav>
       <main className="p-6">
